0587b2f1e2557e46c712cff471ecdc013544daa9
damian <damiangalvan66@gmail.com>
2023-02-13 12:27:37 -0600

    add docker

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:19-alpine3.15 as dev-deps
+WORKDIR /app
+COPY package.json package.json
+RUN npm install
+
+FROM node:19-alpine3.15 as builder
+WORKDIR /app
+COPY --from=dev-deps /app/node_modules ./node_modules
+COPY . .
+# RUN yarn test
+RUN npm run build
+
+FROM node:19-alpine3.15 as prod
+EXPOSE 3000
+WORKDIR /app
+COPY --from=dev-deps /app/node_modules ./node_modules
+COPY --from=builder /app/build ./build
+CMD [ "node","build/index.js"]
--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export const connectDB = async () => {
   try {
     mongoose.set('strictQuery', false) // to avoid deprecation warning
-    const conn = await mongoose.connect('mongodb://localhost:27017/users_db')
+    const conn = await mongoose.connect('mongodb://damian:password@mongo:27017/users_db?authSource=admin');
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
     console.log(error)
